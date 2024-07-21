6c8d77b7525d1c7b0f9442abcf34f049b5920282
J2DCB <junhyeok1209@outlook.kr>
2024-07-22 01:56:49 +0900

    기초 Camera 코드 숙지.공부중
    ROS 관련 종속성 등을 ros 패키지에서 수령해야 하는 걸로 파악.
    
    CV_CAP 관련 변수가 ROS capture 도구 안에 있는지 확인할 것.

--- Camera.cpp
+++ Camera.cpp
@@ -1,43 +1,56 @@
+//
+//  카메라를 CAMERA_INDEX로부터 호출하고, 해당 데이터를 전송하는 코드
+//
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+
+// Dependencies 오류 (아직 코드 실행 불가)
+
+
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
+
+using namespace cv;
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "image_publisher");
     ros::NodeHandle nh;
 
-    // Open camera with CAMERA_INDEX (webcam is typically #0).
     const int CAMERA_INDEX = 0;
     cv::VideoCapture capture(CAMERA_INDEX);
     if (not capture.isOpened()) {
-        ROS_INFO_STREAM("Failed to open camera with index " << CAMERA_INDEX << "!");
+        ROS_INFO_STREAM("Failed to open camera Number :: [ " << CAMERA_INDEX << " ] !"); // 카메라 오픈 (CAMERA_INDEX )
         ros::shutdown();
     }
     // Set the parameters using the results of executing the following command:
     //   v4l2-ctl -d /dev/video0 --list-formats-ext
+    //
     capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
     capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
     capture.set(CV_CAP_PROP_FPS, 10);
-    //double width = capture.get(CV_CAP_PROP_FRAME_WIDTH);
-    //double height = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
-    //ROS_DEBUG_STREAM("width: " << width << ", height: " << height);
+
+
 
     image_transport::ImageTransport it(nh);
     image_transport::Publisher pub_image = it.advertise("camera_raw", 1);
 
     // for debug
-    cv::namedWindow("Image_Publisher", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
+    cv::namedWindow("이미지 게시", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
 
     cv_bridge::CvImagePtr frame = boost::make_shared< cv_bridge::CvImage >();
-    frame->encoding = sensor_msgs::image_encodings::BGR8;
+    frame->encoding = sensor_msgs::image_encodings::BGR8; // frame read (BGR8Format)
     while (ros::ok()) {
         // for debug
         cv::Mat image;
         capture >> image;
-        cv::imshow("Image_Publisher", image);
+        cv::imshow("이미지 게시", image);
         //
 
         capture >> frame->image;
